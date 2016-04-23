7e98821fd51fac60e3d8a65e2096792bbc36a6f8
caryt <cary.thomson@gmail.com>
2016-04-24 07:23:11 +1000

    Move colors to CSS

--- ui/donut.js
+++ ui/donut.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import {PieChart, Pie, Cell, Tooltip as _Tooltip} from 'recharts';
 
-export const Donut = ({children, colors, Tooltip,
+export const Donut = ({children, Tooltip,
  cx, cy, innerRadius, outerRadius, width, height}) => 
     <PieChart width = {width} height = {height}>
         <Pie data = {children} cx = {cx} cy = {cy} 
          innerRadius = {innerRadius} outerRadius = {outerRadius}>
             {children.map((entry, index) => 
-	            <Cell key = {`cell-${index}`} fill = {colors[entry.name]} />
+	            <Cell 
+	            	key = {`cell-${index}`} 
+	            	className = {entry.name}
+            	/>
 	        )}
 	    </Pie>
 	    <_Tooltip content = {<Tooltip/>}/>
