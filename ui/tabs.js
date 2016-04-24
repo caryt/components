f40ae26f1478c9e0b12afe4d055dfdaef0f96bfb
caryt <cary.thomson@gmail.com>
2016-04-24 19:14:56 +1000

    Add Pills components

--- ui/tabs.js
+++ ui/tabs.js
@@ -5,22 +5,37 @@ export const Tabs = ({children}) =>
         {children}
     </ul>;
 
-export const Tab = ({children, id, active}) => 
-    <li role="presentation" className={name(active)}>
+export const Tab = ({children, id, active, className=''}) => {
+    const classes = `${Active(active)} ${className}`;
+    return <li role="presentation" className={classes}>
         <a href={`#${id}`} aria-controls={id} role="tab" data-toggle="tab">
-        	{children}
-    	</a>
+            {children}
+        </a>
     </li>;
+}
 
 export const Panels = ({children}) =>
 	<div className="tab-content">
 		{children}
 	</div>;
 
-export const Panel = ({children, id, active}) => 
-    <div role="tabpanel" className={`tab-pane ${name(active)}`} id={id}>
+export const Panel = ({children, id, active}) => {
+    const classes = `tab-pane ${Active(active)}`;
+    return <div role="tabpanel" className={classes} id={id}>
         {children}
-    </div>;
+    </div>
+};
 
-const name = active =>
-	active ? 'active' : ''
+export const Pills = ({children, stacked, className=''}) => {
+    const classes = `nav nav-pills ${Stacked(stacked)} ${className}`;
+    return <ul className={classes} role="tablist">
+        {children}
+    </ul>;
+}
+
+const Active = active =>
+    active ? 'active' : ''
+
+const Stacked = stacked =>
+    stacked ? 'nav-stacked' : ''
+
