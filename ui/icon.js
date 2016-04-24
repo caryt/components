ded265e4dd98484773260d4236358ce523c763fd
caryt <cary.thomson@gmail.com>
2016-04-24 20:20:21 +1000

    Add arrow icons

--- ui/icon.js
+++ ui/icon.js
@@ -1,4 +1,9 @@
 import React from 'react';
 
-export const Icon = ({fa, className=''}) =>
-    <i className={`fa fa-${fa} ${className}`}/>;
+export const Icon = ({fa=null, arrow=null, className=''}) => {
+    if (fa) {
+    	return <i className={`fa fa-${fa} ${className}`}/>;
+	} else if (arrow) {
+    	return <i className={className}>{arrow}</i>;
+	}
+}
