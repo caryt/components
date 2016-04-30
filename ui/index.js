87278ce34375510aa07f8532af46a0e3518425c1
caryt <cary.thomson@gmail.com>
2016-04-30 20:35:03 +1000

    Add Link class (to make use of Bootstrap's active link class)

--- ui/index.js
+++ ui/index.js
@@ -9,6 +9,7 @@ export * from './forms';
 export * from './grid';
 export * from './i18n';
 export * from './icon';
+export * from './link';
 export * from './list';
 export * from './loading';
 export * from './nav';
---
+++ ui/link.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Link as RawLink} from 'react-router';
+
+export const Link = ({ to, className = null, children }) =>
+    <RawLink to={to} className={className} activeClassName="active">
+        {children}
+    </RawLink>;
