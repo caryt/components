e606ccddcfa106d26891950f93609edfa3cb5119
caryt <cary.thomson@gmail.com>
2016-04-30 13:11:06 +1000

    Add Model to encapsulate Business Logic, e.g. derived fields, from API calls.

--- api/reducers.js
+++ api/reducers.js
@@ -1,8 +1,8 @@
-import { URL, CREATE, READ, UPDATE, DELETE } from './actions';
-import { filter } from 'reframed/functional';
+import { CREATE, READ, UPDATE, DELETE } from './actions';
+import { filter, forEach } from 'reframed/functional';
 
 export function rest(state, action) {
-    const { url, ...keys} = state;
+    const { url, ...keys } = state;
     switch (action) {
     case CREATE:
         return post(url, keys);
@@ -22,16 +22,45 @@ function get(url, keys, options = {}) {
         .set('Content-Type', 'application/json');
 }
 
-/** Helper function to spread response fields in custom CHANGE_xxx reducers.
- *  If the CHANGE action has an error, it returns initial values
- * otherwise it returns values from the response -- both filtered by FIELDS
- * the next action is set to a null action (otherwise we get an infinite loop)
- * and any error is propogated to the result.
+
+/* A Model encapsulates Business Logic and are typically used in reducers that
+ * return state from an external API.
+ *
+ * Subclass and add getter functions to calculate derived fields.
+ * Reducers can return Model instances rather than simple mappings.
 **/
-export const spread = ({ action, FIELDS, INITIAL_STATE }) => ({
-    ...filter((action.error) ? INITIAL_STATE : action, field =>
-        FIELDS.indexOf(field) > -1
-    ),
-    action: INITIAL_STATE.action,
-    error: action.error,
-});
+export class Model {
+    constructor(props) {
+        forEach(props, (key, value) => { this[key] = value; });
+    }
+
+    /** FIELDS are the data (i.e. non-key) fields returned from an API call.
+     *  This method must be overridden in subclasses.
+    **/
+    static get FIELDS() {
+        return [];
+    }
+
+    /** INITIAL_STATE defines the inital values for all fields in the model.
+     *  This method must be overridden in subclasses.
+    **/
+    static get INITIAL_STATE() {
+        return {};
+    }
+
+    /** Helper function to spread response fields in custom CHANGE_xxx reducers.
+     *  If the CHANGE action has an error, it returns initial values
+     * otherwise it returns values from the response -- both filtered by FIELDS
+     * the next action is set to a null action (to prevent an infinite loop)
+     * and any error is propogated to the result.
+    **/
+    static spread(action, FIELDS, INITIAL_STATE) {
+        return {
+            ...filter((action.error) ? INITIAL_STATE : action, field =>
+                FIELDS.indexOf(field) > -1
+            ),
+            action: INITIAL_STATE.action,
+            error: action.error,
+        };
+    }
+}
