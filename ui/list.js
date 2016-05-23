81585d9761bb56ee768f3e8d0ec82041dde4cf58
caryt <cary.thomson@gmail.com>
2016-05-23 16:10:43 +1000

    Implement different List types (Tabular, Ordered, Unordered)

--- ui/list.js
+++ ui/list.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import { Table } from './table';
 
-export const List = ({ list, header, children }) =>
-    <Table header={header}>
+export const TabularList = ({ list, header, children }) =>
+    <Table className="list" header={header}>
         {list.map(children)}
     </Table>;
+
+export const OrderedList = ({ list, header, children }) =>
+    <div className="list">
+        {header}
+        <ol>
+            {list.map(children)}
+        </ol>
+    </div>;
+
+export const UnorderedList = ({ list, header, children }) =>
+    <div className="list">
+        {header}
+        <ul>
+            {list.map(children)}
+        </ul>
+    </div>;
