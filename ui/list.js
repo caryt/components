869714f879c71f561bba40eacbb2c5bc37de043c
caryt <cary.thomson@gmail.com>
2016-05-25 19:28:36 +1000

    Pass arbitrary properties to List types

--- ui/list.js
+++ ui/list.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import { Table } from './table';
 
-export const TabularList = ({ list, header, children }) =>
-    <Table className="list" header={header}>
+export const TabularList = ({ list, header, children, ...rest }) =>
+    <Table className="list" header={header} {...rest}>
         {list.map(children)}
     </Table>;
 
-export const OrderedList = ({ list, header, children }) =>
+export const OrderedList = ({ list, header, children, ...rest }) =>
     <div className="list">
         {header}
-        <ol>
+        <ol {...rest}>
             {list.map(children)}
         </ol>
     </div>;
 
-export const UnorderedList = ({ list, header, children }) =>
+export const UnorderedList = ({ list, header, children, ...rest }) =>
     <div className="list">
         {header}
-        <ul>
+        <ul {...rest}>
             {list.map(children)}
         </ul>
     </div>;
